f837d3fa8c54f02f0c386c62610d5ea10124b936
catsudon <6532180221@student.chula.ac.th>
2026-02-03 19:41:45 +0700

    k

--- checker.py
+++ checker.py
@@ -18,7 +18,7 @@ PRODUCTS = {
     "CC02": "https://www.toylaxy.com/en/product/1227227/product-1227227?category_id=125777",
     # "SL02": "https://www.toylaxy.com/en/product/1273208/product-1273208?category_id=137697",
     "BT09": "https://www.toylaxy.com/en/product/1246962/product-1246962?category_id=137697",
-    "BT08": "https://www.toylaxy.com/en/product/1227221/product-1227221?category_id=125777"
+    # "BT08": "https://www.toylaxy.com/en/product/1227221/product-1227221?category_id=125777"
 }
 
 STATE_FILE = "state.csv"
